44d20d1cf538fd2dc8030539444e9fed2e1dfaae
Вячеслав Сидоров <v.sidorov@intaro.email>
2025-03-27 20:44:20 +0400

    fix

--- dev/assets/scripts/modules/sliderBlock.js
+++ dev/assets/scripts/modules/sliderBlock.js
@@ -1,54 +1,56 @@
-import { Swiper } from '../../../../node_modules/swiper/swiper-bundle.min.mjs'
-import { Pagination } from 'swiper/modules';
+import {Swiper} from '../../../../node_modules/swiper/swiper-bundle.min.mjs'
+import {Pagination, EffectCoverflow} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
 
 export default function sliderBlock() {
-    const slider = document.querySelector('.slider .swiper')
+  const slider = document.querySelector('.slider .swiper')
 
-    if (!slider) return
+  if (!slider) return
 
-    const swiper = new Swiper(slider, {
-        modules: [Pagination],
-        slidesPerView: 'auto',
-        spaceBetween: 20,
-        autoHeight: true,
-        speed: 1000,
-        observer: true,
-        observeParents: true,
-        watchSlidesProgress: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
+  const swiper = new Swiper(slider, {
+    modules: [Pagination, EffectCoverflow],
+    effect: 'coverflow',
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    speed: 1000,
+    coverflowEffect: {
+      rotate: 70,
+      stretch: -100,
+      depth: 500,
+      slideShadows: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    on: {
+      slideChange: function () {
+        const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+        bullets.forEach((bullet, index) => {
+          bullet.classList.remove('swiper-pagination-bullet-active');
+          if (index === this.realIndex) {
+            bullet.classList.add('swiper-pagination-bullet-active');
+          }
+        });
+      }
+    },
+    breakpoints: {
+      1281: {
+        pagination: false,
+        spaceBetween: 0,
+        coverflowEffect: {
+          rotate: 0,
+          stretch: -1000,
+          depth: 2000,
+          slideShadows: false,
         },
-        on: {
-            slideChange: function () {
-                const bullets = document.querySelectorAll('.swiper-pagination-bullet');
-                bullets.forEach((bullet, index) => {
-                    bullet.classList.remove('swiper-pagination-bullet-active');
-                    if (index === this.realIndex) {
-                        bullet.classList.add('swiper-pagination-bullet-active');
-                    }
-                });
-            }
-        },
-        breakpoints: {
-            1281: {
-                pagination: false,
-                spaceBetween: 0
-            }
-        }
-    })
 
-    if(window.innerWidth > 1280) {
-        setTimeout(() => {
-            swiper.slideNext();
-            setTimeout(() => {
-                swiper.slidePrev();
-                setTimeout(() => slider.style.opacity = 1, 1000);
-            }, 500);
-        }, 0);
+      }
     }
+  })
 }
 
